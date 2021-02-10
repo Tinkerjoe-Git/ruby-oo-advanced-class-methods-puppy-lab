5efd079674824871c42fe0184748aa085b6a026c
Tinkergit <nicholas.stagno@gmail.com>
2021-02-10 17:48:08 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,39 @@
-# Add your code here
+class Dog
+    @@all=[]
+
+    attr_accessor :name
+
+
+
+    def initialize(name)
+        @name=name
+        self.save
+    end
+
+    def self.print_all
+        @@all.each do |whatever|
+            puts whatever.name
+        end
+    end
+
+    def self.clear_all
+        @@all=[]
+    end
+
+    def save
+        @@all << self
+    end
+
+
+
+
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+end
